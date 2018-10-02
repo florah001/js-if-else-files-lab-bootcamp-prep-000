767f5d459b2a38493c789be2b62f23527616151d
florah001 <florah001@yahoo.co.uk>
2018-10-02 04:44:28 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,10 +1,7 @@
 var name = "Joe"
 var height = "74"
-if (name === "susan") {
-  console.log("The name 
-  is correct")
-} else
-    console.log("Expected the name to be Susan")
+} else 
+{console.log("Expected the name to be Susan")
 }
 
 
