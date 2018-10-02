5a572011ee623aa6e0ac301e27ad9adbfe9c0330
florah001 <florah001@yahoo.co.uk>
2018-10-02 04:48:14 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 var name = "Joe"
 var height = "74"
-} else 
-{console.log("Expected the name to be Susan")
+if (name === "Susan") {
+    console.log("The name is correct")
+} else {
+    console.log("Expected the name to be Susan")
 }
 
 
